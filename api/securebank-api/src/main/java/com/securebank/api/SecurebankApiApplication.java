8c1e318908766cbc783a56881211d460358802f9
vangetirajkumar-repo <rajkumar@Rajkumarhp.localdomain>
2026-07-19 13:20:30 +0000

    Add Spring Boot API project with Dockerfile

---
+++ api/securebank-api/src/main/java/com/securebank/api/SecurebankApiApplication.java
@@ -0,0 +1,13 @@
+package com.securebank.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurebankApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurebankApiApplication.class, args);
+	}
+
+}
---
+++ api/securebank-api/src/test/java/com/securebank/api/SecurebankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.securebank.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurebankApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
