3690c81f59b78e24207889fc8f05623933c01c1d
vangetirajkumar-repo <rajkumar@Rajkumarhp.localdomain>
2026-07-25 10:14:26 +0000

    Main tf updated

--- main.tf
+++ main.tf
@@ -27,6 +27,11 @@ module "eks" {
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
+  # Explicit endpoint access settings
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = false
+  cluster_endpoint_public_access_cidrs = ["0.0.0.0/0"]
+
   eks_managed_node_groups = {
     default = {
       min_size       = 1
