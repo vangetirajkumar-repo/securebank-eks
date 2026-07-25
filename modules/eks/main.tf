54c393a88ce51afab9d36eeb0a24610968cf5b99
vangetirajkumar-repo <rajkumar@Rajkumarhp.localdomain>
2026-07-25 09:53:01 +0000

    EKS main updated

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -3,7 +3,7 @@ module "eks" {
   version = "20.37.2"
 
   cluster_name    = "securebank-eks"
-  cluster_version = "1.29"
+  cluster_version = "1.35"
 
   subnet_ids = module.vpc.private_subnets   #  connect VPC outputs here
 
